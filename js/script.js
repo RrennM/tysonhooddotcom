b2018780792c0b7e1a7277babe6fe7df0e58c66b
Tyson Hood <TysonHood@gmail.com>
2018-01-11 21:42:33 -0800

    Add thumbs. Adjust Heights. Add services section.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Portfolio section funcitonality
+
+// Find items used for Portfolio thumbnails
+var buttons = document.getElementsByTagName("a");
+var buttonsLength = buttons.length;
+
+// Use a for loop to find the id of each portfolio item
+for (i = 0; i < buttonsLength; i++) {
+  var buttonClicked = document.getElementById('this' + i.toString());
+  // console.log("myPopup" + i.toString());
+  
+  // Add an event listener to determine which Portfolio item has been clicked
+  buttonClicked.addEventListener("click", function() {
+    // console.log(this.id);
+    var r = this.children[1];
+    // console.log(r.id);
+    var popup = document.getElementById(r.id);
+    popup.classList.toggle("show");
+  });
+} 
+
+// End Portfolio Section 
